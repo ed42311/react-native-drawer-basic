bf2c553b9c17336d7430bd14987d27a63a7aa58d
ed42311 <ed42311@gmail.com>
2018-11-16 14:17:27 -0700

    basic draw buitl with mocked screens

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, View, StatusBar } from 'react-native'
+import AppNavigation from './navigation/AppNavigation'
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle='light-content' />
+        <AppNavigation />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+})
